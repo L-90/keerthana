60b38b59ce44b1fc88966234691ee602892a9e22
KEERTHANA S <keerthikeero97@gmail.com>
2017-04-18 10:12:09 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int i,n;
+    printf("\n enter the value");
+    scanf("%d",&n);
+    for(i=0;i<5;i++)
+    {
+
+        printf("\n HELLO");
+    }
+    getch();
+}
