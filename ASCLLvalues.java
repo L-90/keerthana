881b9eb627ee02a14efa8ec34025d31bc51a05d9
KEERTHANA S <keerthikeero97@gmail.com>
2017-06-29 12:12:51 +0530

    Add files via upload

---
+++ ASCLLvalues.java
@@ -0,0 +1,18 @@
+package set3;
+
+import java.util.Scanner;
+
+public class ASCLLvalues {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int x='a';
+		for(int i=0;i<26;i++)
+		{
+			System.out.println(x);
+			x++;
+		}
+		s.close();
+	}
+		
+	}
