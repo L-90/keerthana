a25d9115df335e845eadad8c10d03c8cda66b8cb
KEERTHANA S <keerthikeero97@gmail.com>
2017-06-29 09:53:31 +0530

    Add files via upload

---
+++ Fibonacciseries.java
@@ -0,0 +1,19 @@
+package set3;
+
+public class Fibonacciseries {
+
+	public static void main(String[] args) {
+		 int n=5, t1 = 0, t2 = 1;
+	        System.out.println("First terms: ");
+
+	        for (int i = 1; i <=n; ++i)
+	        {
+	            System.out.println(t1);
+
+	            int sum = t1 + t2;
+	            t1 = t2;
+	            t2 = sum;
+	        }
+	    }
+	}
+
