a4c2de8ce72f718df34f8521072a3ef822a3bb4a
KEERTHANA S <keerthikeero97@gmail.com>
2017-07-01 19:31:14 +0530

    Add files via upload

---
+++ StrintoInt.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class StrintoInt {
+	public static void main(String[] args) {
+		String s="13255";
+		String s1="88";
+		int x=Integer.parseInt(s);
+		int y=Integer.parseInt(s1);
+		System.out.println(x);
+		System.out.println(y);
+		
+		
+
+	}
+
+}
