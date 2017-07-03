74de16b4c75013032aac9b79d1291f0b3c82dbae
KEERTHANA S <keerthikeero97@gmail.com>
2017-07-03 17:01:56 +0530

    Add files via upload

---
+++ Countchar.java
@@ -0,0 +1,16 @@
+package set3;
+
+import java.util.Scanner;
+public class Countchar {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the string");
+		Scanner in=new Scanner(System.in);
+		String s=in.nextLine();
+		System.out.println("no of char is"+s.length());
+	    in.close();
+		}
+
+	}
+
+
