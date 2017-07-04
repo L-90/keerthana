c5555b0d5918f37763d75a78e932303d27d8ca3f
KEERTHANA S <keerthikeero97@gmail.com>
2017-07-04 11:39:53 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,32 @@
+package set3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Prime {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	        System.out.print("\nEnter the range : ");
+	        int range=Integer.parseInt(br.readLine());
+	        System.out.println();
+	        for(int i=2;i<=range;i++)
+	        {
+	            int flag=0;
+	            for(int j=2;j<i;j++)
+	            {
+	                if(i%j==0)
+	                {
+	                    flag=1;
+	                }
+	            }
+	            if(flag==0)
+	                System.out.print(i+" ");
+	        }
+	        System.out.println();
+	 
+	    }
+	
+	}
+
+
