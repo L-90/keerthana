b1b298f6defdba586f51f60ef1c3bd2a74e1454d
KEERTHANA S <keerthikeero97@gmail.com>
2017-07-08 12:00:07 +0530

    Add files via upload

---
+++ Prgm1.java
@@ -0,0 +1,13 @@
+package guviprgm;
+
+public class Prgm1 {
+	public static void main(String[] args) {
+		int x='a';
+		for(int i=0;i<=26;i++) {
+			System.out.println(x);
+			x++;
+			
+		}
+	}
+
+}
