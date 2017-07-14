d9b3065fba6c892cca13614582a7eb30da9bf44d
KEERTHANA S <keerthikeero97@gmail.com>
2017-07-14 09:36:32 +0530

    Add files via upload

--- Maxstring.java
+++ Maxstring.java
@@ -1,5 +1,21 @@
 package gvi;
 
-public class Maxstring {
+import java.util.Scanner;
 
-}
+public class Maxstring {
+	public static void main(String[] args){
+		Scanner input=new Scanner(System.in);
+		String str=input.nextLine();
+		StringBuffer rev=new StringBuffer(str);
+		if(rev.reverse().toString().equals(str)){
+		System.out.println("Maximum substring length :"+(str.length()-1));
+		}
+		else{
+		System.out.println("Maximum substring length :"+str.length());
+		}
+		input.close();
+	}
+
+	}
+
+
