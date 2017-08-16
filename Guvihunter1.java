84070f9cd17bad2eaa73a6c35d29603774f3570b
KEERTHANA S <keerthikeero97@gmail.com>
2017-08-16 10:15:44 +0530

    Add files via upload

---
+++ Guvihunter1.java
@@ -0,0 +1,27 @@
+package program;
+
+import java.util.Scanner;
+
+public class Guvihunter1 {
+	public static void main(String[] args){
+    String str="";
+    Scanner sc=new Scanner(System.in);
+    str=sc.next();
+      for(int i=0;i<str.length();i++){
+    	  System.out.println(str.charAt(0)+""+str.charAt(i));
+    	  
+   
+    	}	
+    			
+    			
+    		}
+     
+    }
+
+    
+    
+	
+
+  
+	
+	
